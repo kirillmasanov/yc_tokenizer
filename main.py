eaa543963a61d0a854e37eaaf2f826c3211af77b
Kirill Masanov <kimasanov@gmail.com>
2026-02-25 00:12:33 +0300

    Запуск uvicorn на порту 8000

--- main.py
+++ main.py
@@ -169,4 +169,4 @@ async def tokenize(req: TokenizeRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
